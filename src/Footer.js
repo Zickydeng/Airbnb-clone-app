2ae456349a8988f97a6a5c20599e0da290b91d18
Abcd <abcd@Zickys-MacBook-Pro.local>
2021-03-31 16:27:01 -0400

    Zicky's v1 commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>© 2021 Elijah's Airbnb clone! No rights reserved - this is a demo-App Created By Zicky!</p>
+            <p>Privacy · Terms · Sitemap · Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
